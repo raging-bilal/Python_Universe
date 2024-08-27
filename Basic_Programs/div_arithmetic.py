6679f7ff919d3bdc0c75fe45be63eaae0e14346b
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-27 22:22:06 +0530

    Add files via upload

---
+++ Basic_Programs/div_arithmetic.py
@@ -0,0 +1,7 @@
+num1=input("Enter the first number: ")
+
+num2=input("Enter the second number: ")
+
+div=float(num1) / float(num2)
+
+print("The quotient of {0} and {1} is {2} ".format(num1,num2,div))
