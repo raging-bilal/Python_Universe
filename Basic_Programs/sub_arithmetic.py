9ba5d11b84442fb7dffdb892b3529bcd61e007b2
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-27 22:26:54 +0530

    Update sub_arithmetic.py

--- Basic_Programs/sub_arithmetic.py
+++ Basic_Programs/sub_arithmetic.py
@@ -4,5 +4,5 @@ num2=input("Enter the second number: ")
 
 sub=float(num1) - float(num2)
 
-print("The result of {0} - {1} is {2}!".format(num1,num2,sub))
+print("The result of {0} - {1} is {2} ".format(num1,num2,sub))
 
