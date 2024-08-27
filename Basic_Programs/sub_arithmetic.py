5a3d15727d258b0c2cdfba9587a1f6d60ea691a3
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-27 22:21:09 +0530

    Add files via upload

---
+++ Basic_Programs/sub_arithmetic.py
@@ -0,0 +1,8 @@
+num1=input("Enter the first number: ")
+
+num2=input("Enter the second number: ")
+
+sub=float(num1) - float(num2)
+
+print("The result of {0} - {1} is {2}!".format(num1,num2,sub))
+
