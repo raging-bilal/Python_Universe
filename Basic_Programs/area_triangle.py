24481f54b6890500882b09c9c634eca0e6afa0fe
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-27 22:41:52 +0530

    Add files via upload

---
+++ Basic_Programs/area_triangle.py
@@ -0,0 +1,12 @@
+a=float(input("Enter the length of first side: "))
+
+b=float(input("Enter the length of second side: "))
+
+c=float(input("Enter the length of third side: "))
+
+s= (a+b+c)/2
+print("The value of s is: ", str(s))
+
+area=(s*(s-a)*(s-b)*(s-c)) **0.5
+
+print('The area of the triangle is %0.2f' %area)
