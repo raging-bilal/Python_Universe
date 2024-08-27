0345880e1d9bd3650ad281d047596125264d7119
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-27 22:21:34 +0530

    Add files via upload

---
+++ Basic_Programs/mul_arithmetic.py
@@ -0,0 +1,8 @@
+num1=input("Enter the first number: ")
+
+num2=input("Enter the second number: ")
+
+mul=float(num1) * float(num2)
+
+print("The product of {0} and {1} is {2}".format(num1,num2,mul))
+
