ed455407cc67f3915a81512b95ed06550eda8118
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-29 23:19:18 +0530

    Add files via upload

---
+++ Basic_Programs/14_randomNumber1.py
@@ -0,0 +1,11 @@
+# 1. Generating a Random Number - Using the random() function
+
+import random
+
+n=random.random()
+
+print("The random number between 0 and 1: ", n)
+
+print("The random number between 0 and 1: ", random.random() )
+
+print("The random number between 0 and 1: ", random.random())
