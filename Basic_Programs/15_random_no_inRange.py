3e39615f67fc887fd15967a054d2d00375d0a116
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-29 23:19:40 +0530

    Add files via upload

---
+++ Basic_Programs/15_random_no_inRange.py
@@ -0,0 +1,11 @@
+# 2. Generating a Number within a Given Range - Using the randint() function
+
+import random
+
+n=random.randint(0,50)
+
+print("Randon number between 0 and 50: ", n)
+
+print("Randon number between 0 and 50: ", random.randint(0,50) )
+
+print("Randon number between 0 and 50: ", random.randint(0,50))
