ff6a0e7e1ddf1e6307ddcf30d47baa60a6758d0d
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-29 23:20:57 +0530

    Add files via upload

---
+++ Basic_Programs/17_random_no_sample.py
@@ -0,0 +1,13 @@
+import random
+
+list1=random.sample(range(10,40),10)
+
+my_list=[11,22,33,44,55,66,77,88,99,00]
+
+list2=random.sample(my_list,5)
+
+print("The 10 random elements of list_1: ", list1)
+
+
+print("The 5 random elements of list_2: ", list2)
+
