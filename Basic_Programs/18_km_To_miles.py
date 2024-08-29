3ce1cd47f95712557ddff94f9c97d640e512914d
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-29 23:37:35 +0530

    Add files via upload

---
+++ Basic_Programs/18_km_To_miles.py
@@ -0,0 +1,10 @@
+# Python program to convert Kilometres to Miles
+
+
+km=float(input("Enter the speed of the vehicle in kilometers (km): "))
+
+f=0.62137
+
+miles= km * f
+
+print("The value of {0} kilometers in miles: {1}".format(km,miles))
