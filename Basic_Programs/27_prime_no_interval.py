c8f87f3823d0198d7ca3b1c348f0ee26d49a09b7
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-01 14:47:56 +0530

    Update 27_prime_no_interval.py

--- Basic_Programs/27_prime_no_interval.py
+++ Basic_Programs/27_prime_no_interval.py
@@ -1,11 +1,18 @@
-lower_value = int(input ("Please, Enter the Lowest Range Value: "))  
-upper_value = int(input ("Please, Enter the Upper Range Value: "))  
-  
-print ("The Prime Numbers in the range are: ")  
-for number in range (lower_value, upper_value + 1):  
-    if number > 1:  
-        for i in range (2, number):  
-            if (number % i) == 0:  
-                break  
-        else:  
-            print (number)
+def checkPrimeInRange(l,u):
+    for i in range(l,u+1):
+        if i >1:
+            for j in range(2,i):
+                if i%j==0:
+                    break
+            else:
+                print(i)
+
+
+l=int(input("Enter the lower limit: "))
+u=int(input("Enter the upper limit: "))
+
+checkPrimeInRange(l,u)
+
+
+
+
