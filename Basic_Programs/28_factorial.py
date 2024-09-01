c383a6af8e9f4bc6435d08344786248127f35e3f
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-01 14:48:20 +0530

    Update 28_factorial.py

--- Basic_Programs/28_factorial.py
+++ Basic_Programs/28_factorial.py
@@ -1,10 +1,21 @@
-num = int(input("Enter a number: "))    
-factorial = 1    
-if num < 0:    
-   print(" Factorial does not exist for negative numbers")    
-elif num == 0:    
-   print("The factorial of 0 is 1")    
-else:    
-   for i in range(1,num + 1):    
-       factorial = factorial*i    
-   print("The factorial of",num,"is",factorial)  
+
+def Findfact(n):
+   fact=1
+   
+
+   if n<0:
+      print(n, " is negative number.So, its factorial doesnot exist!")
+      
+
+   elif n==0:
+      print("The factorial of ",n," is 1!")
+
+   else:
+      
+      for i in range(1,n+1):
+         fact=fact*i
+         print(fact)
+   
+n=int(input("Enter the number to find the factorial: "))
+
+Findfact(n)
