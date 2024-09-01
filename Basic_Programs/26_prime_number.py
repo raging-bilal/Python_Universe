363360d287a4edeb745ab632f529a7f2ec0eb70a
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-01 14:47:19 +0530

    Update 26_prime_number.py

--- Basic_Programs/26_prime_number.py
+++ Basic_Programs/26_prime_number.py
@@ -1,20 +1,19 @@
 # A default function for Prime checking conditions  
-def PrimeChecker(a):  
-    # Checking that given number is more than 1  
-    if a > 1:  
-        # Iterating over the given number with for loop  
-        for j in range(2, int(a/2) + 1):  
-            # If the given number is divisible or not  
-            if (a % j) == 0:  
-                print(a, "is not a prime number")  
-                break  
-        # Else it is a prime number  
-        else:  
-            print(a, "is a prime number")  
-    # If the given number is 1  
-    else:  
-        print(a, "is not a prime number")  
-# Taking an input number from the user  
-a = int(input("Enter an input number:"))  
-# Printing result  
-PrimeChecker(a)  
+
+
+def checkPrime(p):
+    if p>1:
+        for i in range(2, int(p/2) +1):
+            if p%i==0:
+                print("{0} is NOT Prime Number !".format(p))
+                break          
+        else:   
+            print("{0} is Prime Number !".format(p))
+
+    else:
+        print("{0} is NOT Prime Number !".format(p))
+
+
+p=int(input("Enter the number to check for the PRIME: "))
+
+checkPrime(p)
