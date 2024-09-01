e8eddc751f13a268470ca63451818e8770d08c50
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-01 14:46:37 +0530

    Update 25_leap_year.py

--- Basic_Programs/25_leap_year.py
+++ Basic_Programs/25_leap_year.py
@@ -1,14 +1,17 @@
-# Default function to implement conditions to check leap year  
-def CheckLeap(Year):  
-  # Checking if the given year is leap year  
-  if((Year % 400 == 0) or  
-     (Year % 100 != 0) and  
-     (Year % 4 == 0)):   
-    print("Given Year is a leap Year");  
-  # Else it is not a leap year  
-  else:  
-    print ("Given Year is not a leap Year")  
-# Taking an input year from user  
-Year = int(input("Enter the number: "))  
-# Printing result  
-CheckLeap(Year) 
+# # Default function to implement conditions to check leap year  
+
+
+
+def checkLeap(y):
+    if (y%400==0)  or ((y%4==0) and (y%100!=0)):
+        print("{0} is the Leap year! ".format(y))
+
+
+
+    else:
+        print("{0} is NOT the Leap year! ".format(y))
+
+
+y=int(input("Enter the year to check for leap Year: "))
+
+checkLeap(y)
