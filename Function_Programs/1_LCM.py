a383fdf991498ca04f651d80d1495f348ce66dc8
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-03 18:18:25 +0530

    Update 1_LCM.py

--- Function_Programs/1_LCM.py
+++ Function_Programs/1_LCM.py
@@ -1,1 +1,24 @@
+def lcm(a,b):
+    if a>b:
+        greater=a
+
+    else:
+        greater=b
+
+
+    while(True):
+        if (greater%a==0) and (greater%b==0):
+            lcm=greater
+            break
+        greater+=1
+
+    print("The LCM of {0} and {1} is {2}! ".format(a,b,lcm))
+
+
+a=int(input("Enter the first number to check the LCM: "))
+b=int(input("Enter the second number to check the LCM: "))
+
+lcm(a,b)
+
+
 
