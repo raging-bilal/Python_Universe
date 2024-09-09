faa1fc348da9c10718db8bf6b25fc338c273f3e9
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-09 22:55:13 +0530

    Add files via upload

---
+++ Array Programs/8_printOddPos.py
@@ -0,0 +1,11 @@
+a=[1,2,3,4,5,6,7,8,9]
+
+print("Printing the original array! ")
+for i in range(0,len(a)):
+    print(a[i],end=" ")
+    
+print()
+
+print("Printing the array elements from odd position only! ")
+for i in range(0,len(a),2):
+    print(a[i],end=" ")
