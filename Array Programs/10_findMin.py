874f6b4fc0b65230644871afe7de0303b458ae67
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-09 22:55:55 +0530

    Add files via upload

---
+++ Array Programs/10_findMin.py
@@ -0,0 +1,19 @@
+a=[32,455,6343,6,32,45,22]
+
+print("Printing the original array! ")
+for i in range(0,len(a)):
+    print(a[i],end=" ")
+    
+print()
+
+print("Printing the smallest array element! ")
+
+
+
+min=a[0]
+for i in range(0,len(a)):
+    
+    if a[i]<min:
+        min=a[i]
+
+print("The smallest value in the array : ",str(min))
