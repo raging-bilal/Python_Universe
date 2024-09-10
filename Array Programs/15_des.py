9c0cb121d78d196735ec0fc1114318bf3c02311a
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-10 20:00:48 +0530

    Add files via upload

---
+++ Array Programs/15_des.py
@@ -0,0 +1,29 @@
+a=[23,4,5,64,2,23,46,7,89,43]
+l=len(a)
+temp=0
+
+print("Print the Original Array ! ")
+for i in range(0,l):
+    print(a[i], end=" ")
+
+print()
+
+
+
+for i in range(0,l):
+    for j in range(i+1,l):
+        if a[i]<a[j]:
+            temp=a[i]
+            a[i]=a[j]
+            a[j]=temp
+
+
+
+print("Print the elements of array in descending order !")
+for i in range(0,l):
+    print(a[i], end=" ")
+
+
+
+
+
