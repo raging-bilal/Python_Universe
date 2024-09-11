b176e0c942b1b4278c2064d1ad3a95f2c605d4ae
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-11 18:57:48 +0530

    Update 1_addMatrices.py

--- Matrix_Program/1_addMatrices.py
+++ Matrix_Program/1_addMatrices.py
@@ -1,1 +1,22 @@
 
+X = [[1,2,3],  
+     [4,5,6],  
+     [7,8,9]]  
+  
+Y = [[10,11,12],  
+     [13,14,15],  
+     [16,17,18]]  
+  
+Result = [[0,0,0],  
+          [0,0,0],  
+          [0,0,0]]  
+
+# iterate through rows  
+for i in range(len(X)):  
+   # iterate through columns  
+   for j in range(len(X[0])):  
+       Result[i][j] = X[i][j] + Y[i][j]  
+
+# print the result
+for r in Result:  
+   print(r)
