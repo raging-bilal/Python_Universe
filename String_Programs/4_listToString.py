3a0a6d94e7e151554525efb233fdc2b36cc5f3dc
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-13 21:34:25 +0530

    Add files via upload

---
+++ String_Programs/4_listToString.py
@@ -0,0 +1,14 @@
+# Convert List to String in Python
+
+# Python program to convert a list to a string by using the join() method  
+  
+# Creating a list with elements of string data type  
+a_list = ["Python", "Convert", "List", "String", "Method"]  
+  
+# Converting to string  
+string = " ".join( a_list ) # this is read as join elements of a_list with a separator (" ")  
+  
+# Printing the string  
+print (string)  
+print (type(string))  
+
