f750e41c4d42b2d08270fd8dc1fcd20dbc25eed2
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-15 17:49:50 +0530

    Add files via upload

---
+++ List_Programs/3_extendElement.py
@@ -0,0 +1,18 @@
+l=[1,2,3,4,5]
+
+print("The original list : ")
+print(l)
+
+
+print("Enter an element to insert in the list:  ")
+
+l.extend([100,200,300,400,500,"Khushnood","Bilal"])
+
+print("The new extended list : ")
+print(l)
+
+
+
+
+
+
