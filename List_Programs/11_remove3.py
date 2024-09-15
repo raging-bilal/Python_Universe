6d528924f2363e97bfb3a01dd572a908d5345751
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-15 17:52:55 +0530

    Add files via upload

---
+++ List_Programs/11_remove3.py
@@ -0,0 +1,9 @@
+# Python program to clear all the elements from the list    
+    
+lst = ["Python", "Remove", "Elements", "List", "Tutorial"]    
+print("Initial List is :", lst)    
+    
+# Using the clear() function    
+element = lst.clear()    
+print(element)    
+print(lst)    
