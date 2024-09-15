c4f6f16081b3f4c34cb83efa038c093a14b3d05c
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-15 17:50:08 +0530

    Add files via upload

---
+++ List_Programs/4_compare1.py
@@ -0,0 +1,18 @@
+# The set() function and == operator
+
+
+l1=[1,2,3,4,5]
+l2=[2,3,4,4,1,2]
+
+l1=set(l1)
+l2=set(l2)
+
+if l1==l2:
+    print("l1 and l2 are equal!")
+else:
+    print("l1 and l2 are not equal!")
+
+
+
+
+    
