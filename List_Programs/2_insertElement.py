4d7ddceb14973943eb485968d09be315b4164a03
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-15 17:49:29 +0530

    Add files via upload

---
+++ List_Programs/2_insertElement.py
@@ -0,0 +1,17 @@
+l=[1,2,3,4,5]
+
+print("The original list : ")
+print(l)
+
+i=int(input("Enter an index to insert element in the list: "))
+n=int(input("Enter an element to insert in the list:  "))
+
+l.insert(i,n)
+
+print("The new inserted list : ")
+print(l)
+
+
+
+
+
