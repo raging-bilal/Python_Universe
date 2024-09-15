fbbf3cf5344ebe84318bd5c3a7b9ff03862779f3
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-15 17:52:09 +0530

    Add files via upload

---
+++ List_Programs/10_remove2.py
@@ -0,0 +1,9 @@
+# Python program to show how to use the pop() function    
+      
+lst = ["Python", "Remove", "Elements", "List", "Tutorial"]    
+print("Initial List is :", lst)    
+      
+# using pop() function to remove element at index 2 of the list    
+element = lst.pop(2)    
+print("Element that is popped :", element)    
+print("List after deleting the element", lst)    
