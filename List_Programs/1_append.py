d27da5fc1a46183ac9cea341907ee9f542139eb8
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-15 17:49:05 +0530

    Update 1_append.py

--- List_Programs/1_append.py
+++ List_Programs/1_append.py
@@ -1,1 +1,15 @@
+l=[1,2,3,4,5]
+
+print("The original list : ")
+print(l)
+
+n=input("Enter an element to append in the list: ")
+l.append(n)
+
+print("The new appended list : ")
+print(l)
+
+
+
+
 
