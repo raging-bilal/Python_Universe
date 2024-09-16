86f3f2af365d049e6d122bb8f7c6f315fb9e0145
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-16 21:11:19 +0530

    Add files via upload

---
+++ List_Programs/20_listToString.py
@@ -0,0 +1,43 @@
+# Methods to Convert List to String in Python
+
+
+# 1. Using join() Method
+
+
+
+# string.join( iterable )  
+
+
+# Python program to convert a list to a string by using the join() method  
+  
+# Creating a list with elements of string data type  
+a_list = ["Python", "Convert", "List", "String", "Method"]  
+  
+# Converting to string  
+string = " ".join( a_list ) # this is read as join elements of a_list with a separator (" ")  
+  
+# Printing the string  
+print (string)  
+print (type(string))  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
