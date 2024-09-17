a936bb081ce7b54bda87f7692020a15ac1ac5a3c
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-17 23:20:46 +0530

    Add files via upload

---
+++ Dictionary_Programs/2_create2.py
@@ -0,0 +1,14 @@
+# Initializing an empty Dictionary  
+Dictionary = {}  
+print("An empty Dictionary: ")  
+print(Dictionary)  
+   
+# Creating a Dictionary using in-built dict() method  
+Dictionary = dict({1: 'Python', 2: 'Javatpoint', 3:'Dictionary'})  
+print("\nDictionary created by using dict() method: ")  
+print(Dictionary)  
+   
+# Creating dictionary by key:value pair format  
+Dictionary = dict([(1, 'Javatpoint'), (2, 'Python'), (3, 'Dictionary')])  
+print("\nDictionary with key:value pair format: ")  
+print(Dictionary)  
